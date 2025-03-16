ba681aaf4bdc1671e4230043090be92a1fb72618
HAFIJ AHMAD <hafijahmad86@yahoo.in>
2025-03-16 05:53:14 +0530

    Addnew Migration
    All netcore data is file modified Migration

---
+++ Migrations/20250225153540_SeedEmployessTable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeMangement.Migrations
+{
+    public partial class SeedEmployessTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "id", "Department", "Email", "Name" },
+                values: new object[] { 1, 2, "mark@pragimtech.com", "Mark" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "id",
+                keyValue: 1);
+        }
+    }
+}
---
+++ Migrations/20250225154626_AlterseedEmployessTable.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeMangement.Migrations
+{
+    public partial class AlterseedEmployessTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "id",
+                keyValue: 1,
+                columns: new[] { "Email", "Name" },
+                values: new object[] { "Mary@pragimtech.com", "Mary" });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "id", "Department", "Email", "Name" },
+                values: new object[] { 2, 1, "John@pragimtech.com", "John" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "id",
+                keyValue: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "id",
+                keyValue: 1,
+                columns: new[] { "Email", "Name" },
+                values: new object[] { "mark@pragimtech.com", "Mark" });
+        }
+    }
+}
